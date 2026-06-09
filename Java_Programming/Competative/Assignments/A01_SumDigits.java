3bc47195a46815cc7045a884934fb1125e814eac
Aishwaryaaj30 <aishwaryajadhav3101@gmail.com>
2026-06-09 17:24:36 +0530

    Add files via upload

---
+++ Java_Programming/Competative/Assignments/A01_SumDigits.java
@@ -0,0 +1,68 @@
+// Write a program to find the sum of digits of a number
+
+/*
+Algorithm
+    START
+        Accept number iNo
+        Initialize iSum = 0
+        While iNo is not equal to 0:
+            Find the last digit : idigit = iNo % 10
+            Add digit to iSum: iSum = iSum + iDigit
+            Remove the last digit: iNo = iNo / 10
+        Display result (iSum)
+    STOP
+ */
+
+///////////////////////////////////////////////////////////
+//
+//  Class name    : Logic  
+//  Function name : SumofDigits
+//  Input         : int
+//  Output        : int
+//  Description   : Perform summation of digits
+//  Date          : 08/06/2026
+//  Author        : Aishwarya Mohan Jadhav
+//
+///////////////////////////////////////////////////////////
+
+class Logic
+{
+    void SumofDigits(
+                        int iNo         // variable to take input
+                    )
+    {
+        int iDigit = 0;                 // Variable for operation
+        int iSum = 0;                   // Variable to store sum of digits
+
+        while(iNo != 0)                 // Logic
+        {
+            iDigit = iNo % 10;
+            iSum = iSum + iDigit;
+            iNo = iNo / 10;
+        }
+
+        System.out.println("Summation of digits is : " + iSum);
+    }
+}
+
+///////////////////////////////////////////////////////////
+//
+//  Application to perform summation of digits
+//
+///////////////////////////////////////////////////////////
+
+class A01_SumDigits
+{
+    public static void main(String A[])
+    {
+        Logic lObj = new Logic();        // Object of Function Logic
+        lObj.SumofDigits(1234);     // Call
+    }
+}
+
+///////////////////////////////////////////////////////////
+//
+//  Input :     1234
+//  Output :    10
+//
+///////////////////////////////////////////////////////////
