5e60343a608066fa00d2da06d95595e5ff42a443
Aishwaryaaj30 <aishwaryajadhav3101@gmail.com>
2026-07-07 20:03:25 +0530

    Add files via upload

---
+++ Java_Programming/Practice/program51.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class NumberX
+{
+    public boolean checkPerfect(int iNo)
+    {
+        
+
+        for()
+    }
+}
+
+class program51
+{
+    public static void main(String A[])
+    {
+
+    }
+}
