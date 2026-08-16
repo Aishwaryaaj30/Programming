7bb457bcd142ba4ff56bc0042101a3c6dfb8da95
Aishwaryaaj30 <aishwaryajadhav3101@gmail.com>
2026-08-17 00:01:14 +0530

    Add files via upload

---
+++ Java_Programming/Competative/Assignments/A79_Electricity.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class A79_Electricity
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int units = 0;
+        int billAmount = 0;
+
+        System.out.println("Enter units consumed : ");
+        units = sobj.nextInt();
+
+        if(units < 0)
+        {
+            System.out.println("Invalid input");
+        }
+        else if(units <= 100)
+        {
+            billAmount = units * 5;
+
+            System.out.println("Total Electricity Bill: ₹" + billAmount);
+        }
+        else if(units <= 200)
+        {
+            billAmount = (100 * 5) + ((units - 100) * 7);
+
+            System.out.println("Total Electricity Bill: ₹" + billAmount);
+        }
+        else
+        {
+            billAmount = (100 * 5) + (100 * 7) + ((units - 200) * 10);
+
+            System.out.println("Total Electricity Bill: ₹" + billAmount);
+        }
+    }
+}
